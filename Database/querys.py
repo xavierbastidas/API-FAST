341213898367fc89bbcf3c0abd670186b3e585c4
xavierbastidas <bastidasjonathan140148@gmail.com>
2023-04-13 11:01:24 -0500

    update

--- Database/querys.py
+++ Database/querys.py
@@ -1,10 +1,9 @@
 queries= {
      "get_customers":"SELECT * FROM Cliente",
-     "add_customer":
-     "INSERT INTO Cliente(nombre,cedula,correo,telefono,fechaRegistro) VALUES (@nombre,@cedula,@correo,@telefono,@fechaRegistro)",
      "get_products":"SELECT * FROM ProductoView  WHERE [stock] != 0",
      "get_product":"SELECT * FROM Producto  where  idProducto = ? and  [stock] != 0  ",
-     "delete_product":"DELETE FROM Producto  where idProducto = ?"
+     "delete_product":"DELETE FROM Producto  where idProducto = ?",
+     "add_new_customer":"INSERT INTO Cliente(nombre,cedula,correo,telefono,fechaRegistro) VALUES (?,?,?,?,?)"
 }
 
 
